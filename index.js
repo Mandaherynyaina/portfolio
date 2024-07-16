f525a1e806bc41a3b35f35e0cbd8f9a3cd863554
Manda Hery Ny Aina <rakotomanda00@gmail.com>
2024-07-16 13:57:27 +0200

    finalisation

--- index.js
+++
@@ -1,6 +0,0 @@
-export function showContent(let content){
-    const box = document.getElementById('box');
-    const newContent = document.createElement('p');
-    newContent.textContent = document.getElementById(content);
-    box.appendChild(newContent);
-}
